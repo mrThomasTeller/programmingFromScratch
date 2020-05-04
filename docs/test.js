3e761fcf8d76ce6c414a40b74fd5c2fef25f7c9e
Artyom Bakharev <abakharev@sharevault.com>
2020-05-04 18:40:32 +0700

    up

--- docs/test.js
+++ docs/test.js
@@ -1,6 +1,17 @@
-let x = 3;
-let result = 1;
-while (result < 1000) {
-  console.log(result);
-  result *= x;
+let a = [1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
+
+let max = 0;
+let length = 0;
+for (let x of a) {
+  if (x === 0) {
+    length += 1;
+  }
+  if (length > max) {
+    max = length;
+  }
+  if (x !== 0) {
+    length = 0;
+  }
 }
+
+console.log(max);
