f94cf081f25a27f85dcf62ef1271b8a0f91c5dee
Artyom Bakharev <abakharev@sharevault.com>
2020-05-24 17:17:05 +0700

    up

--- test/_test.js
+++ test/_test.js
@@ -1,29 +1,40 @@
-const fs = require("fs");
 const readlineSync = require("readline-sync");
+let str = readlineSync.question("str\n> ");
+let alph = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let strNew = "";
 
-let nameStr = readlineSync.question("name?\n> ");
-let year = parseInt(readlineSync.question("year?\n> "));
-let newBook = {
-  name: nameStr,
-  year: year,
-  read: false,
-};
-
-let books = JSON.parse(fs.readFileSync("books.json"));
-let index = -1;
-let minYear = Infinity;
-
-for (let i = 0; i < books.length; ++i) {
-  if (books[i].year < minYear) {
-    minYear = books[i].year;
-    index = i;
+for (let i = 0; i < str.length; ++i) {
+  if ((str[i - 1] === " " || str[i - 1] === "") && alph.includes(str[i])) {
+    strNew += str[i].toUpperCase();
+  } else {
+    strNew += str[i];
   }
 }
-
-if (index !== -1) {
-  books.splice(index, 1);
-}
-
-books.push(newBook);
-
-fs.writeFileSync("books.json", JSON.stringify(books));
+console.log(strNew);
