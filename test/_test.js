416987b4aa0a3124eab47db6dea9ec8602211fe9
Artyom Bakharev <abakharev@sharevault.com>
2020-05-26 18:55:11 +0700

    up

--- test/_test.js
+++ test/_test.js
@@ -1,40 +1,4 @@
-const readlineSync = require("readline-sync");
-let str = readlineSync.question("str\n> ");
-let alph = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-let strNew = "";
-
-for (let i = 0; i < str.length; ++i) {
-  if ((str[i - 1] === " " || str[i - 1] === "") && alph.includes(str[i])) {
-    strNew += str[i].toUpperCase();
-  } else {
-    strNew += str[i];
-  }
-}
-console.log(strNew);
+let a = [1, 2, 3];
+let b = a.concat();
+b[0] = 10;
+console.log(a);
